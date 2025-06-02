cd43d947dc2be87d126c59a97ceebc726dee418e
Imaran-khan <imrankhan958585@gmail.com>
2025-06-02 16:48:48 +0530

    go

---
+++ get.js
@@ -0,0 +1,22 @@
+function createCounter(){
+    let count=0;
+    
+
+return {
+    increment:function(){
+        count+=1;
+        return count;
+    },
+    getCount:function(){
+        return count;
+    }
+}
+}
+
+const counter = createCounter();
+
+console.log(counter.increment());
+
+console.log(counter.increment()); 
+
+console.log(counter.getCount()); 
