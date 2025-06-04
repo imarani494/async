60c5076331169b9b37da11c8925cf0deb394c28a
Imaran-khan <imrankhan958585@gmail.com>
2025-06-04 10:28:16 +0530

    setTimeout

---
+++ setTime.js
@@ -0,0 +1,20 @@
+
+
+setTimeout(() => {
+    console.log("Message 1");
+}, );
+
+
+setTimeout(() => {
+
+    console.log("Message2");
+    
+}, 2000);
+
+
+
+setTimeout(() => {
+
+    console.log("Message3");
+    
+}, );
