bec8626649c05d23ace710380b61511888982149
Imaran-khan <imrankhan958585@gmail.com>
2025-06-04 12:26:25 +0530

    timer

---
+++ asTimer.js
@@ -0,0 +1,13 @@
+function timer(duration,onComplete){
+
+
+    setTimeout(()=>{
+        onComplete(`Timer of ${duration} ms finished`);
+
+    },duration)
+}
+
+timer(3000,(message)=>{
+    console.log(message);
+
+});
